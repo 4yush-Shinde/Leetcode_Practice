6bcb7bdaaa47c94f9ea914e67080ca62e4ecb928
Ayush Shinde <shindeayush1504@gmail.com>
2026-07-31 10:16:36 +0530

    Time: 386 ms (93.33%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
 SELECT w1.id as Id
 FROM Weather w1
 JOIN Weather w2
-ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+ON w1.recordDate - INTERVAL 1 DAY = w2.recordDate
 WHERE w1.temperature > w2.temperature;
